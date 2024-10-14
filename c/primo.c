67487772fd6ef29ad3a7e68ebaeb1b45568773fc
Amrsolo <amrahmed5125@gmail.com>
2024-10-14 09:39:11 +0000

    Ciao Mondo

---
+++ c/primo.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+/*
+Scaricare ed installare nel proprio computer il software Dev C++.
+Scrivere ed eseguire con Dev C++ questo semplice programma CiaoMondo.c
+
+
+*/
+
+int main() 
+{
+  int a;
+  
+  printf("Inserisci un numero intero ");
+  scanf("%d", &a);
+  
+  printf("Hai inserito il numero %d\n", a);
+ 
+  return 0;
+}
+  
