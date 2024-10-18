01ad552b2a28c2f4d6fb7d694a5d06a2ed81a020
Amrsolo <amrahmed5125@gmail.com>
2024-10-18 20:23:18 +0000

    nuovo programma

---
+++ La selezione/bonus.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+int main()
+{   
+    //vf=voto finale
+    int vf,crediti;
+    float media;
+
+    printf("inserisci la tua media \n");
+    scanf("%f",&media);
+    printf("insersici quanti credeti hai \n");
+    scanf("%d",&crediti);
+
+    if(media>=28)
+    {
+        if(crediti>24)
+        {
+            vf=media+2;
+            printf("hai recevuto un bonus di 2 voti ,adesso tua media= %.2f",media);
+        }
+        else
+        {
+            printf("non pastano i tuoi crediti\n");
+        }
+        
+    }
+    if(media>=25)
+    {
+        if(media<=27)
+        {
+            if(crediti<=24)
+            {
+                if(crediti>=18)
+                {
+                    vf=media+1;
+                    printf("hai recevuto un bonus di 1 voti ,adesso tua media= %.2f \n",media);
+                }
+                else
+                {
+                     printf("non pastano i tuoi crediti\n");
+                }
+            }
+            else
+            {
+            printf("non pastano i tuoi crediti\n");
+            }
+        }
+        else
+        {
+            printf("non pasta la tua media\n");
+        }
+        
+
+    }
+    if(media<=25)
+    {
+        if(crediti<18)
+        {
+           
+            printf("il voto finale rimane invariato\n");
+        }
+        else
+        {
+            printf("non pasta la tua media\n");
+        }
+    }
+    return 0;
+}
