461154d9912e4db054fd074685ae90aa511a3056
Amrsolo <amrahmed5125@gmail.com>
2024-11-06 18:03:46 +0000

    UPDATE

--- La selezione/letto un carattere.c
+++ La selezione/letto un carattere.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 int main()
 
+
 {
     char a;
+
 
     printf("inserisci una lettera \n");
     scanf("%c",&a);
 
+
     a=("%d",a);
-    
-    if(a<=90)
-    {   
+   
+    if(a<=90&&a>=65)
+    {  
         a=a+32;
         printf("la lettera era in maioscola e adesso sara' cosi' %c \n",a);
     }
-    else if(a>=97)
+    else if(a>=97&&a<=122)
     {
         a=a-32;
-        printf("la lettera era in minoscolo e adesso sara' cosi' %c \n",a);
+        printf("la lettera era in maioscolo e adesso sara' cosi' %c \n",a);
     }
     else
     {
-        printf("non è una lettera");
+        printf("non è una lettera \n");
     }
     return 0;
 }
+
