d87f92aa7e46aaff6382feb167b3e0fbb683dfba
Amrsolo <amrahmed5125@gmail.com>
2024-11-27 12:02:32 +0000

    update

--- c/Cicli for/e.c
+++ c/Cicli for/e.c
@@ -8,18 +8,16 @@ li visualizzi tutti (nel ciclo) e stampi la loro somma (fuori dal ciclo).
 int main()
 {
     int i,a,somma=0;
-
     printf("inserisci 10 numeri\n");
 
-    
-    for(i=0;i<10;i++)
+    for(i=1;i<10;i++)
     {
-        
         scanf("%d",&a);
         somma+=a;
+        printf("i numeri che hai inseritto sono %d\n",a);
     }
     
     printf(" la somma = %d\n",somma);
-    printf("i numeri che hai inseritto sono %d",a);
+    
     return 0;
 }
