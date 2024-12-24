23a486aca6417ddfe347389c594ba6db48db3d86
Amrsolo <amrahmed5125@gmail.com>
2024-12-24 23:32:52 +0000

    update

--- c/ Le funzioni/tutto.c
+++ c/ Le funzioni/tutto.c
@@ -67,9 +67,23 @@ void cifra_1(int x)
     num+=x%10;
     printf("il numero dell'ultima cifra = %d\n",num);
 }
+void somma_cifre(int x,int num1)
+{
+    /*Scrivi una funzione che trovi la somma
+     delle cifre di un dato numero intero.
+      Ad esempio, la somma delle cifre di 123 è 6.
+    */
+    int i,somma=0;
+    for(i=1;i<=num1;i++)
+    {
+        somma+=x%10;
+        printf("la somma = %d\n",somma);
+    }
+    
+}
 int main()
 {
-    int a,b,c;
+    int a,b,c,d;
 
     printf("inserici il primo numero \n");
     scanf("%d",&a);
@@ -77,12 +91,15 @@ int main()
     scanf("%d",&b);
     printf("inserici un numero lungo almeno 3 cifre per trovare l'ultima cifra \n");
     scanf("%d",&c);
+    printf("inserici quante cifre hai messe adesso \n");
+    scanf("%d",&d);
 
     area_rettangolo(a,b);
     mcd(a,b);
     somma_divisori(a);
     pari_dispari(a);
     cifra_1(c);
+    somma_cifre(c,d);
     return 0;
 }
 
